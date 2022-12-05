be380b679fa1e099bfe4d8397a12872582b41d9e
arthurolivieri <arthuro@al/insper.edu.br>
2022-12-05 15:40:54 -0300

    Caso 3, linhas 22, 23, 25, 26, 27 corrigidas

---
+++ js/calendario_compartilhado.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    d_ana = document.querySelector('#dona_ana')
+    console.log(d_ana)
+    d_ana.addEventListener('click', function(){
+        window.location = "filtre_dona_ana.html"
+    })
+})
